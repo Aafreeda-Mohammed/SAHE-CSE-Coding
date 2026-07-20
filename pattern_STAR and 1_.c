30a45e45758b1d30c74a98202bb6d66b1db23512
Aafreeda-Mohammed <aafreedamohammed@gmail.com>
2026-07-20 20:42:55 +0530

    Added STAR and 1 pattern
    Hardcoded pattern using printf . Building consistency

---
+++ pattern_STAR and 1_.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	printf("* * * * * * *\n");
+	printf("*     1     *\n");
+	printf("*     1     *\n");
+	printf("*     1     *\n");
+	printf("*     1     *\n");
+	printf("*   1 1 1   *\n");
+	printf("* * * * * * *\n");
+	return 0;
+	
+}
