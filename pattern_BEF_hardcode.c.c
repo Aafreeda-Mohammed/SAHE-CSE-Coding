77f9479d0ea4ea09b1ace0472f1c8f4b39c20e98
Aafreeda-Mohammed <aafreedamohammed@gmail.com>
2026-07-20 20:24:58 +0530

    Added BEF Pattern
    Learning basics first

---
+++ pattern_BEF_hardcode.c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	printf("BBB  EEEEE  FFFFF\n");
+	printf("B B  E    F\n");
+	printf("B B  E    F\n");
+	printf("BBB  EEE  FFF\n");
+	printf("B B  E    F\n");
+	printf("B B  E    F\n");
+	printf("BBB  EEEEE  F\n");
+	return 0;
+}
