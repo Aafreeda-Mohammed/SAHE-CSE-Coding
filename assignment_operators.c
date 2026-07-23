f1dbe2b45fae66ec1b42255472cda8eba29d6dc8
Aafreeda-Mohammed <aafreedamohammed@gmail.com>
2026-07-23 20:08:43 +0530

    Add assignment operators
    Add a program that demonstrates the assignment operators using C

---
+++ assignment_operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,n=5,b=2;
+	printf("%d\n",a);
+	printf("%d\n",a+=10);
+	printf("%d\n",a-=10);
+	printf("%d\n",a*=n+1);
+	printf("%d\n",a%=b);
+	return 0;
+}
