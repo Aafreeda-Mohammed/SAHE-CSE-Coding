e9dfc92a8e852bb06a88a4620d53a72b53192b97
Aafreeda-Mohammed <aafreedamohammed@gmail.com>
2026-07-30 22:35:23 +0530

    Add explicit type C program 2

---
+++ expilcit_type 2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=100,c=1000,d=10000,e=100000;
+	printf("a=%d,b=%d,c=%d,d=%d,e=%d",a,b,c,d,e);
+	printf("a=%f,b=%f,c=%f,d=%f,e=%f",(float)a,(float)b,(float)c,(float)d,(float)e);
+	return 0;
+}
