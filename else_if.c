481719468cd100479eb9cb34d1b0aba657cd29b0
Aafreeda-Mohammed <aafreedamohammed@gmail.com>
2026-08-08 19:03:28 +0530

    Add else_if.c
    This program demonstrates the use of else-if ladder 
    By using else-if ladder we can find out the given number is +ve,-ve or zero

---
+++ else_if.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("n is positive");
+	}
+	else if(n<0)
+	{
+		printf("n is negative");
+	}
+	else
+	{
+		printf("n is zero");
+	}
+	return 0;
+}
