1f1ad6f2a760e1405db5258cb39dfa81ce0bdd05
Aafreeda-Mohammed <aafreedamohammed@gmail.com>
2026-08-08 18:36:48 +0530

    Add if_else_.c
    This program demonstrates if-else statement
    With the use of if-else statement we can compare two numbers

---
+++ if_else_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	if(a==b)
+	{
+		printf("both a and b are equal");
+	}
+	else
+	{
+		printf("a and b are no equal");
+	}
+	return 0;
+}
