1f464aa699a285f5820b5f210bb3dfa413ac98ae
Aafreeda-Mohammed <aafreedamohammed@gmail.com>
2026-08-08 17:43:15 +0530

    Update arithematic_operators.c

--- 01_Basics/arithematic_operators.c
+++ 01_Basics/arithematic_operators.c
@@ -4,7 +4,7 @@ int main()
 	int a,b;
 	printf("Enter a and b values");
 	scanf("%d %d",&a,&b);
-	printf("sum=%d\n",a+b);
+	printf("sum=%d\n",a+b);arithematic_operators.c
 	printf("diff=%d\n",a-b);
 	printf("product=%d\n",a*b);
 	if(b !=0) {
