2e108bd8243eaddf2524d73fb5b2740c65e0d340
Aafreeda-Mohammed <aafreedamohammed@gmail.com>
2026-08-08 18:57:13 +0530

    Add if_else_6.c
    This program demonstrates the use of if-else statement
    By using if statement we can determine whether the given number is divisible by 5 and 11 or not

---
+++ if_else_6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%5==0&&a%11==0)
+	{
+		printf("the given number is divisible by both 5 and 11");
+	}
+	else
+	{
+		printf("the given number is not divisible by 5 and 11");
+	}
+	return 0;
+}
