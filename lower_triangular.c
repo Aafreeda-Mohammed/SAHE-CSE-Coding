e50e85214d8e12b7b06e20cf2210e2c439f6fab4
Aafreeda-Mohammed <aafreedamohammed@gmail.com>
2026-09-06 16:49:47 +0530

    Add lower_triangular.c
    This program determines whether the entered matrix is lower triangular or not

---
+++ lower_triangular.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,c=1;
+	printf("Enter the n value : ");
+	scanf("%d",&n);
+	int a[n][n];
+	printf("Enter the elements of the matrix : ");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i<j)
+			{
+				if(a[i][j]!=0)
+				{
+					c=0;
+				}
+			}
+		}
+	}
+	if(c==1)
+	{
+		printf("Lower triangular");
+	}
+	else
+	{
+		printf("Not Lower triangular");
+	}
+	return 0;
+}
